2e141e4614b43cda05816c21ddc715a4ea042c45
Shiv-96 <kshiv.dot@gmail.com>
2022-11-08 19:49:25 +0530

    JDBC Connection successful

---
+++ MGNREGA/src/com/project/utility/DBUtill.java
@@ -0,0 +1,34 @@
+package com.project.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtill {
+	
+	public static Connection provideConnection() {
+		
+		Connection conn = null;
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String url = "jdbc:mysql://localhost:3306/MGNREGA";
+		
+		try {
+			conn = DriverManager.getConnection(url, "root", "shiv");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return conn;
+		
+	}
+	
+}
