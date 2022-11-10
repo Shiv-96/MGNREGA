766277a656bd834a3f48f6010de421426bb190d1
Shiv-96 <kshiv.dot@gmail.com>
2022-11-10 13:12:50 +0530

    JDBC Connection Update

--- MGNREGA/src/com/mgnrega/utility/DBUtill.java
+++ MGNREGA/src/com/mgnrega/utility/DBUtill.java
@@ -3,25 +3,39 @@ package com.mgnrega.utility;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class DBUtill {
+	
+	private static String url;
+	private static String driverName;
+	private static String username;
+	private static String password;
+	
+	static {
+		
+		ResourceBundle rb = ResourceBundle.getBundle("MGNREGADB");
+		
+		url = rb.getString("db.url");
+		driverName = rb.getString("db.drivername");
+		username = rb.getString("db.username");
+		password = rb.getString("db.password");
+		
+	}
+	
 	
 	public static Connection provideConnection() {
 		
 		Connection conn = null;
 		
 		try {
-			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
+			Class.forName(driverName);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		
-		String url = "jdbc:mysql://localhost:3306/MGNREGA";
-		
 		try {
-			conn = DriverManager.getConnection(url, "root", "shiv");
+			conn = DriverManager.getConnection(url, username, password);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
